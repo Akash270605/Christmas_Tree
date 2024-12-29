8ab6887b50f479c2c55688a60985aee67bdc9dc5
Akash Kumar <akashkr270605@gmail.com>
2024-12-29 23:40:44 +0530

    Add files via upload

---
+++ Christmas/src/christmas/Ornament.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package christmas;
+
+
+//************************************************************************
+// Author: Chisheng Li
+// Class Name: Ornament.java
+//
+// Description of the class: This class defines an ornament as a decorative oval. 
+// Each ornament will be the same shape, but the color and placement can vary. 
+//
+// Supplementary files: TreePanel.java is needed to plot the ornaments.
+//*************************************************************************
+
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class Ornament extends JPanel{
+	private int xPos, yPos;
+	private Color clr;
+	private int WIDTH = 12;
+	
+	public Ornament(){
+		Random r = new Random();
+		xPos = r.nextInt(250);
+		yPos = r.nextInt(250);
+		clr = Color.yellow;		
+	}
+	
+	public Ornament(int x, int y, Color c){
+		xPos = x;
+		yPos = y;
+		clr = c;
+	}
+	
+	public void draw(Graphics g){
+		g.setColor(clr);
+		g.fillOval (xPos, yPos, WIDTH, WIDTH);
+	}
+}
+
